2bf50dff77c3a45528971a89f04a279f5769c080
alsalgom88 <asalido2@xtec.cat>
2025-11-05 10:24:41 +0100

    Primera entrada

---
+++ main.js
@@ -0,0 +1,7 @@
+// 🧠 Petit script per afegir interacció
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnWelcome");
+  btn.addEventListener("click", () => {
+    alert("🚀 Benvingut/da al repositori GS_DAW2A_0612! Recorda fer push sovint 😄");
+  });
+});
